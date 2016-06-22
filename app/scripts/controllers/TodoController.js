4b6785d49a61b30a9429fd78877553a3dac2f06f
Raduan <raduan.santos@softplan.com.br>
2016-06-22 15:19:32 -0300

    Einstein era genio...

--- app/scripts/controllers/TodoController.js
+++ app/scripts/controllers/TodoController.js
@@ -3,19 +3,25 @@
 angular.module('todoApp')
     .controller('TodoController', ['$scope', function ($scope) {
 
-        $scope.todos = [ {name: 'TODO 1', description: 'Todo numero 1', done: false},
-            {name: 'TODO 2', description: 'Todo numero 2',done: false}];
+        $scope.description = '';
 
-        $scope.add = function(todo) {
-            if(!todo.name.length || !todo.description.length) {
+        $scope.todos = [{description: 'Todo numero 1', finished: false},
+            {description: 'Todo numero 2', finished: false}];
+
+        this.clear = function () {
+            $scope.description = '';
+        };
+
+        $scope.add = function () {
+            if (!$scope.description.length) {
                 //TODO - mensagem de erro
                 return;
             }
-            $scope.todos.push(todo);
+            $scope.todos.push({description: $scope.description, finished: false});
+            clear();
         };
 
-        $scope.delete = function($index) {
-            $scope.todos.splice($index, 1)
+        $scope.delete = function ($index) {
             //mensagem de deletado;
         };
 
