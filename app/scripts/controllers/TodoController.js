73e5bedf4e947c18244e930209ef177d34d66ac0
Raduan <raduan.santos@softplan.com.br>
2016-06-22 14:00:55 -0300

    Adicionado esqueleto - pelos poderes de grayskull !!!!

--- app/scripts/controllers/TodoController.js
+++ app/scripts/controllers/TodoController.js
@@ -3,5 +3,20 @@
 angular.module('todoApp')
     .controller('TodoController', ['$scope', function ($scope) {
 
-        $scope.toto = 'tata';
+        $scope.todos = [ {name: 'TODO 1', description: 'Todo numero 1', done: false},
+            {name: 'TODO 2', description: 'Todo numero 2',done: false}];
+
+        $scope.add = function(todo) {
+            if(!todo.name.length || !todo.description.length) {
+                //TODO - mensagem de erro
+                return;
+            }
+            $scope.todos.push(todo);
+        };
+
+        $scope.delete = function($index) {
+            $scope.todos.splice($index, 1)
+            //mensagem de deletado;
+        };
+
     }]);
